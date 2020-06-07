995ecbe1dc4036ec7df8c100ffddfe9b587de81c
Lewis <13007790+The-Neco@users.noreply.github.com>
2020-06-07 03:00:50 +0100

    Added comments and fixed spelling mistakes

--- CustomPlate/plate.lua
+++ CustomPlate/plate.lua
@@ -1,9 +1,9 @@
 --- IMAGE CONFIG HERE ---
-imageUrl = "https://i.imgur.com/30nUtFe.png" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl = "Insert URL Here" -- Paste your image URL here (doesn't have to be from imgur)
 
 
 -- The actual script --
-local textureDic = CreateRuntimeTxd('duiTxd') -- Create custom texture dictionary
+local textureDic = CreateRuntimeTxd('duiTxd') -- Create custom texture dictionary only needs to be done once
 local object = CreateDui(imageUrl, 540, 300) -- Load image into object
 local handle = GetDuiHandle(object) -- Gets DUI handle from object
 CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
@@ -14,12 +14,11 @@ AddReplaceTexture('vehshare', 'plate04', 'duiTxd', 'duiTex') -- Applies "duiTex"
 AddReplaceTexture('vehshare', 'plate05', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate05" from "vehshare"
 
 
-local object = CreateDui('https://i.imgur.com/Q3uw6V7.png', 540, 300)
-local handle = GetDuiHandle(object)
-CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle)
-local textureDic = CreateRuntimeTxd('duiTxd')
-AddReplaceTexture('vehshare', 'plate01_n', 'duiTxd', 'duiTex')
-AddReplaceTexture('vehshare', 'plate02_n', 'duiTxd', 'duiTex')
-AddReplaceTexture('vehshare', 'plate03_n', 'duiTxd', 'duiTex')
-AddReplaceTexture('vehshare', 'plate04_n', 'duiTxd', 'duiTex')
-AddReplaceTexture('vehshare', 'plate05_n', 'duiTxd', 'duiTex')
+local object = CreateDui('https://i.imgur.com/Q3uw6V7.png', 540, 300) -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate01_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate01_n" from "vehshare"
+AddReplaceTexture('vehshare', 'plate02_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate02_n" from "vehshare"
+AddReplaceTexture('vehshare', 'plate03_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate03_n" from "vehshare"
+AddReplaceTexture('vehshare', 'plate04_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate04_n" from "vehshare"
+AddReplaceTexture('vehshare', 'plate05_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate05_n" from "vehshare"
